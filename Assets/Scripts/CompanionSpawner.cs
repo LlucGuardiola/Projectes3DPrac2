a446b58a9e54a59fd2d6d4519ab6c614e174cd3a
apolo1516 <pueblopalote@gmail.com>
2025-11-12 22:00:58 +0100

    Boton va pero es cutre

--- Assets/Scripts/CompanionSpawner.cs
+++ Assets/Scripts/CompanionSpawner.cs
@@ -3,17 +3,37 @@ using UnityEngine;
 public class CompanionSpawner : MonoBehaviour
 {
     public GameObject m_CompanionCubePrefab;
-    public Transform m_SpawnerTransform; 
+    public Transform m_SpawnerTransform;
+
+    private bool m_PlayerInZone = false;
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if (other.CompareTag("Player"))
+        {
+            m_PlayerInZone = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            m_PlayerInZone = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (m_PlayerInZone && Input.GetKeyDown(KeyCode.E))
         {
             Spawn();
         }
     }
+
     void Spawn()
     {
-        var l_GameObject = GameObject.Instantiate(m_CompanionCubePrefab);
+        var l_GameObject = Instantiate(m_CompanionCubePrefab);
         l_GameObject.transform.position = m_SpawnerTransform.position;
         l_GameObject.transform.rotation = m_SpawnerTransform.rotation;
         l_GameObject.transform.localScale = new Vector3(.75f, .75f, .75f);
