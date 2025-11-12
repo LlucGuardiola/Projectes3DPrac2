75e022b251c555cfbd91c24186a85a43bfadefe6
LlucGuardiola <arceus88205@gmail.com>
2025-11-12 18:33:06 +0100

    commit

--- Assets/Scripts/PortalButton.cs
+++ Assets/Scripts/PortalButton.cs
@@ -3,13 +3,22 @@ using UnityEngine.Events;
 
 public class PortalButton : MonoBehaviour
 {
-    public UnityEvent m_Event;
+    public UnityEvent m_Open;
+    public UnityEvent m_Close;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Cube"))
         {
-            m_Event.Invoke();
+            m_Open.Invoke();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Cube"))
+        {
+            m_Close.Invoke();
         }
     }
 }
